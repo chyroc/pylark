1045640de12494c42485e67dd680bf74735c713b
chyroc <chyroc@qq.com>
2021-05-31 15:32:45 +0800

    init package

---
+++ pylark/__init__.py
@@ -0,0 +1,7 @@
+"""Feishu/Lark Open API Python Sdk, Support ALL Open API and Event Callback."""
+
+from pylark.lark import Lark
+
+__version__ = "0.0.3"
+
+__all__ = ['Lark']
---
+++ pylark/lark.py
@@ -0,0 +1,6 @@
+class Lark(object):
+    def __init__(self):
+        pass
+
+    def test(self):
+        return 'test'
